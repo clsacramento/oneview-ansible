c43fb686e0481c310037d674a80f112e69acdc22
Cynthia Lopes do Sacramento <clsacramento@gmail.com>
2017-11-10 15:16:18 -0500

    simplifies the file

--- delete_esx.sh
+++ delete_esx.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 server_profile_name="esx-synergyhost-04"
-provisioned_nic="ManagementNIC"
-hostname_field="Hostname"
 post_provisioning_tasks="../config_tasks/remove_host_from_vcenter.yaml"
 
-#playbooks/get_provisioning_info.yaml
-ansible-playbook -i hosts playbooks/remove_provisioned_server.yaml -e server_profile_name=$server_profile_name -e provisioned_nic=$provisioned_nic -e hostname_field=$hostname_field -e post_provisioning_tasks=$post_provisioning_tasks 
+ansible-playbook -i hosts playbooks/remove_provisioned_server.yaml -e server_profile_name=$server_profile_name -e post_provisioning_tasks=$post_provisioning_tasks 
